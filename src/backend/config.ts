c2288528262e3aa741b2ef72a17bfb146a915539
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-08-09 18:02:07 -0300

    fix: env var names

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -3,9 +3,9 @@ import { getFirestore } from 'firebase/firestore';
  
 // Configuração do app firebase web
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_CRUD_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_CRUD_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_CRUD_PUBLIC_FIREBASE_PROJECT_ID,
 }
  
 // Inicializando firebase
